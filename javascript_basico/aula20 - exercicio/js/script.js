3ec734b8cc133234e0f706b0f782777d82544099
Isabele Rocha <isabeledpr@gmail.com>
2023-07-12 01:41:42 -0300

    site para calcular IMC

--- javascript_basico/aula20 - exercicio/js/script.js
+++ javascript_basico/aula20 - exercicio/js/script.js
@@ -26,7 +26,7 @@ function meuEscopo() {
         });
         
         console.log(pessoas);
-        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value} </p>`
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value} </p>]`
 
     }
 
